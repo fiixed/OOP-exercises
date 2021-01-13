ec39da9ecc91774654e178d84046587c02e35bab
fiixed <fiixed@gmail.com>
2021-01-13 09:59:35 -0500

    added node for module import

--- Card.js
+++ Card.js
@@ -8,26 +8,4 @@ class Card {
     }
   }
 
-  class Hand {
-      constructor() {
-          this.hand = [];
-      }
-
-      addCard(Card){
-        this.hand.push(Card);
-      }
-      getPoints() {
-        if (this.hand.length == 0) {
-            console.log(0);
-        } else {
-            let totalPoints = this.hand.reduce((prev, current) => {
-                return prev.point + current.point;
-            });
-            console.log(totalPoints);
-        }
-      }
-  }
-var myHand = new Hand()
-myHand.addCard(new Card(5, 'diamonds'))
-myHand.addCard(new Card(13, 'spades'))
-myHand.getPoints()
+ export default Card;
---
+++ Hand.js
@@ -0,0 +1,25 @@
+import Card from './Card.js';
+
+class Hand {
+    constructor() {
+        this.hand = [];
+    }
+
+    addCard(Card){
+      this.hand.push(Card);
+    }
+    getPoints() {
+      if (this.hand.length == 0) {
+          console.log(0);
+      } else {
+          let totalPoints = this.hand.reduce((prev, current) => {
+              return prev.point + current.point;
+          });
+          console.log(totalPoints);
+      }
+    }
+}
+var myHand = new Hand();
+myHand.addCard(new Card(5, 'diamonds'));
+myHand.addCard(new Card(13, 'spades'));
+myHand.getPoints();
